9991c1122b5abf31fec3b52b0c110f6e694669d1
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2021-11-05 16:16:57 +0100

    Improved admin design & added remove button for cards

--- prototype/public/js/firebase.js
+++ prototype/public/js/firebase.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCIy172a-eDssYLn7uTCPvUlHLzrtOvV5s",
+    authDomain: "morphocards-3976f.firebaseapp.com",
+    projectId: "morphocards-3976f",
+    storageBucket: "morphocards-3976f.appspot.com",
+    messagingSenderId: "1048733341127",
+    appId: "1:1048733341127:web:39a31dd14a61a4f6d2b5e2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore();
 
 // Récupération des collections :

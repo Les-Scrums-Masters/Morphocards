9dc11e541a7133c274785713a0cb268d9d726efb
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2021-11-18 10:22:48 +0100

    Created react project for morphocard administration (with Tailwind CSS)

---
+++ morphocards_admin/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['Plus Jakarta Sans']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

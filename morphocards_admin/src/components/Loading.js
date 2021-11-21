c278c7974de6f0a611f1e23ab95bbd430439591f
Franck G <fgutmann@etu.unistra.fr>
2021-11-21 15:50:11 +0100

    Improved Loading component design

--- morphocards_admin/src/components/Loading.js
+++ morphocards_admin/src/components/Loading.js
@@ -5,8 +5,9 @@ export default class Loading extends React.Component {
     render() {
         return(
             <div className="loading">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
                 <p>Chargement ...</p>
             </div>

182e4125912ecf2d438f809d835794b5c181d55a
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2021-11-26 16:41:21 +0100

    Implemented TTS (to test on a server)

--- morphocards/src/components/card.jsx
+++ morphocards/src/components/card.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import * as googleTTS from 'google-tts-api';
+import {Howl} from 'howler';
+
 
 export default class Card extends React.Component{
-    state={
-        value: this.props.value
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick = (e) =>{
-        console.log(e.target);
-        //SON A JOUER LORS DU CLICK
+    componentDidMount() {
+        let url = googleTTS.getAudioUrl(this.props.value, {
+            lang: 'fr-FR',
+            slow: false,
+            host: 'https://translate.google.com',
+          });
+
+        this.sound = new Howl({
+            src: [url],
+            format: ['mp3']
+        });
+
+        this.sound.once('load', function(){
+            this.sound.play();
+          });          
+
+    }
+
+    handleClick(e) {
+        console.log(this.sound);
+        this.sound.play();
     }
 
     render (){
@@ -26,7 +49,7 @@ export default class Card extends React.Component{
                     {...provided.draggableProps} 
                     {...provided.dragHandleProps}
                     >
-                        <h3>{this.state.value}</h3>
+                        <h3>{this.props.value}</h3>
                     </div>
                 )}
             </Draggable>

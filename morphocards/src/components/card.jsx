5bbb39fafa66fe895fa29c838615212e14027208
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2021-11-30 17:06:37 +0100

    Working Text to spreech, cleaned code & dependancies

--- morphocards/src/components/card.jsx
+++ morphocards/src/components/card.jsx
@@ -1,53 +1,30 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
-import {Howl} from 'howler';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 
-export default class Card extends React.Component{
+export default function Card(props) {
 
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    componentDidMount() {
-        
-        this.sound = new Howl({
-            src: ['http://morphocards.franck-g.fr/tts/'+this.props.value],
-            format: ['mp3']
-        });
-
-        this.sound.once('load', function(){
-            this.sound.play();
-          });          
-
-    }
-
-    handleClick(e) {
-        console.log(this.sound);
-        this.sound.play();
-    }
-
-    render (){
-        return (
-            <Draggable
-                //key={this.props.id}
-                draggableId={this.props.id}
-                index={this.props.index}
+    const { speak } = useSpeechSynthesis();
+   
+    return (
+        <Draggable
+        draggableId={props.id}
+        index={props.index}
+    >
+        {provided => (
+            <div
+            className="card draggable"
+            onClick={() => speak({ 
+                text: props.id, 
+            })}
+            ref={provided.innerRef}
+            {...provided.draggableProps} 
+            {...provided.dragHandleProps}
             >
-                {provided => (
-                    <div
-                    className="card draggable"
-                    onClick={this.handleClick}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps} 
-                    {...provided.dragHandleProps}
-                    >
-                        <h3>{this.props.value}</h3>
-                    </div>
-                )}
-            </Draggable>
-            
-        )
-    }
-}
+                <h3>{props.value}</h3>
+            </div>
+        )}
+    </Draggable>
+    );
+  }

d6e1ed2fd7870761422dc5f09b4125250a7eb997
Franck G <fgutmann@etu.unistra.fr>
2021-12-12 20:09:09 +0100

    Small design improvements

--- morphocards/src/components/RoundButton.jsx
+++ morphocards/src/components/RoundButton.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import 'animate.css';
 
 export default function RoundButton(props) {
 
-    let className = ((props.animated) ? "animate__animated animate__pulse animate__infinite" : "")
+    let className = ((props.animated) ? "animate-pulse" /*"animate__animated animate__pulse animate__infinite"*/ : "")
 
     return(
-        <div className="btnGroup">
+        <div className="flex flex-col justify-center gap-3 md:gap-3 items-center text-center mt-5">
             <button onClick={props.onClick} className={"roundBtn " + className}>{props.children}</button>
-            <label>{props.label}</label>
+            <label className="w-16 md:w-44 text-center font-medium uppercase text-white text-opacity-60 text-xs md:text-base h-8 md:h-16 break-words select-none">{props.label}</label>
         </div>
     );
 

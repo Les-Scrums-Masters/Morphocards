1ece525bd4179cb7f00db857e823cb153a2153c0
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2021-12-17 01:19:51 +0100

    Fixed Javascript warning

--- morphocards/src/components/AppLogo.jsx
+++ morphocards/src/components/AppLogo.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 
+
+/**
+ * Logo de l'application
+ *
+ * @component AppLogo
+ *
+ * @param   {function} onClick  Fonction qui s'execute lorsqu'on clique sur le logo
+ *
+ * @example
+ * <AppLogo onClick={props.openMenu} />
+ *
+ * @return {JSX} Le rendu jsx du logo de l'application.
+ */
+
 function AppLogo(props) {
 
     return (
         <button onClick={props.onClick}>
 
             <h3 className="text-2xl text-white text-opacity-50 hover:text-opacity-100 transition ease-out duration-200">
-                
+
                 <span className="font-extrabold">Morpho</span>
                 cards
             </h3>
